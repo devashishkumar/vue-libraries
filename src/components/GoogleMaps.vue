ca57763eb93d9d11406709147a5a31f759e9e5f1
arora.ashish0512 <arora.ashish0512@gmail.com>
2021-01-16 12:36:15 +0530

    vue js google maps marker integration

---
+++ src/components/GoogleMaps.vue
@@ -0,0 +1,124 @@
+<template>
+  <div class="hello">
+    <div :id="divId" class="ashish-google-maps">maps</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+declare const google: any;
+@Options({
+  data() {
+    return {
+      divId: "",
+      customMarkers: []
+    };
+  },
+  props: {
+    markers: [],
+    latLong: {}
+  },
+  created() {
+    this.divId = this.generateDynamicString(8);
+    setTimeout(() => {
+      this.loadMap();
+    }, 0);
+  },
+  methods: {
+    loadMap() {
+      const googleInit = this.getGoogleInitData(
+        this.latLong.lat,
+        this.latLong.long
+      );
+      this.nMap = new google.maps.Map(
+        document.getElementById(this.divId),
+        googleInit
+      );
+      this.renderMarkers(this.latLong);
+    },
+    getGoogleInitData(lattitude: string, longtitude: string) {
+      return {
+        zoom: 16,
+        center: new google.maps.LatLng(lattitude, longtitude),
+        styles: [
+          {
+            featureType: "administrative.country",
+            elementType: "geometry",
+            stylers: [{ visibility: "simplified" }, { hue: "#ff0000" }]
+          }
+        ]
+      };
+    },
+    renderMarkers(event: any) {
+    const lat = event.long;
+    const lng = event.long;
+    const R = this.radius; // radius of earth in km
+    
+    // window.console.log(this.nMap.marker, '70');
+    // return;
+    for (let i = 0; i < this.markers.length; i++) {
+      const currentLat = this.markers[i].lat;
+      const currentLong = this.markers[i].long;
+      const currentType = this.markers[i].type;
+
+      const marker = new google.maps.Marker({
+        position: new google.maps.LatLng(currentLat, currentLong),
+        map: this.nMap,
+        label: {
+          text: currentType,
+          fontWeight: 'normal',
+          fontSize: '14px',
+          color: 'white'
+        }
+      });
+
+      marker.setValues({ id: i, type: currentType });
+			((marker, l) => {
+				google.maps.event.addListener(marker, 'click', () => {
+          // window.console.log(l, this.markers[l], '119');
+          this.markerClickEvent.emit({rowClicked: l});
+				});
+			})(marker, i);
+			this.customMarkers.push(marker);
+    }
+  },
+
+    generateDynamicString(length: number) {
+      let result = "";
+      const characters =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+      const charactersLength = characters.length;
+      for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
+      }
+      return result;
+    },
+  },
+})
+export default class GoogleMaps extends Vue {
+  msg!: string;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+.ashish-google-maps {
+    position: inherit !important;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <GoogleMaps
+      @markerClicked="clickMarkerMethod($event)"
+      :markers="markers"
+      :latLong="latLong"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import GoogleMaps from "@/components/GoogleMaps.vue"; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld
-  }
+    GoogleMaps,
+  },
+  data() {
+    return {
+      latLong: {
+        lat: 28.5636,
+        long: 77.3726,
+      },
+      markers: [
+        {
+          lat: 28.5661,
+          long: 77.3698,
+          type: "Shiv Mandir Barola",
+        },
+        {
+          lat: 28.5666,
+          long: 77.3664,
+          type: "Budh Bazaar",
+        },
+        {
+          lat: 28.5636,
+          long: 77.3656,
+          type: "Prayag Hospital",
+        },
+        {
+          lat: 28.5579,
+          long: 77.3726,
+          type: "Shri Ram Hospital",
+        },
+        {
+          lat: 28.564,
+          long: 77.378,
+          type: "Post Office Barola",
+        },
+      ],
+    };
+  },
 })
 export default class Home extends Vue {}
 </script>
